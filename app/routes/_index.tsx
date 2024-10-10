a5773f145078a5d972f2fc171ddd4a5f87c643b3
zczy <92977807+zczyk@users.noreply.github.com>
2024-10-10 14:24:05 +0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -41,7 +41,7 @@ export default function Index() {
                 to="/generate-image"
                 className="block w-full text-center px-6 py-3 text-lg font-semibold text-white bg-gradient-to-r from-indigo-500 to-indigo-700 rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-400"
               >
-                白嫖 CF 的 Flux 生成图片
+                Flux 生成图片
               </Link>
             </li>
             <li>
